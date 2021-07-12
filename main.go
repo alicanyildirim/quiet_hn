780bba7a317282ed0fea7af129c9d9fbcfe86a2a
Alican Yıldırım <alicany6m@gmail.com>
2021-07-12 15:53:29 +0300

    a race condition

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		stories, err := getTopStories(numStories)
+		stories, err := getCachedStories(numStories)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -50,6 +50,23 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
+var (
+	cache []item
+	cacheExpiration time.Time
+)
+func getCachedStories(numStories int) ([]item, error) {
+	if time.Now().Sub(cacheExpiration) < 0 {
+		return cache, nil
+	}
+
+	stories, err := getTopStories(numStories)
+	if err != nil {
+		return nil, err
+	}
+	cache = stories
+	cacheExpiration = time.Now().Add(1 * time.Second)
+	return cache, nil
+}
 func getTopStories(numStories int) ([]item, error){
 	var client hn.Client
 	ids, err := client.TopItems()
@@ -67,7 +84,6 @@ func getTopStories(numStories int) ([]item, error){
 }
 
 func getStories(ids []int) []item {
-	var client hn.Client
 	type result struct {
 		index int
 		item item
@@ -76,6 +92,7 @@ func getStories(ids []int) []item {
 	resultCh := make(chan result)
 	for index := 0; index < len(ids); index++ {
 		go func(index, id int) {
+			var client hn.Client
 			hnItem, err := client.GetItem(id)
 			if err != nil {
 				resultCh <- result{index: index, err: err}
